fe525b61b9197b371dd312ba8c438c0e2afa924b
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-06-08 23:43:55 +0100

    fixed some layout setting

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
     <div>
       <h1  class="text-2xl"><span class="text-rose-400">Jok</span><span class="text-blue-500">er4</span><span class="text-green-300">mas</span> &#x1F921;</h1>
     </div>
-    <nav class="hidden lg:flex flex-row gap-4 mr-4">
+    <nav class="hidden lg:flex lg:flex-row gap-4 mr-4">
       <router-link to="/" class="focus:border-b-2 border-green-400 font-bold">Home</router-link>
       <router-link to="/about" class="focus:border-b-2 border-green-400 font-bold">About</router-link>
       <router-link to="/contact" class="focus:border-b-2 border-green-400 font-bold">Contact</router-link>
       <router-link to="/notFound" class="focus:border-b-2 border-green-400 font-bold">404-page</router-link>
     </nav>
-    <div></div>
+    <div class="hidden"></div>
   </div>
   <hr class="">
   <div class="container">
