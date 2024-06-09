eb2241583c1f573b182254590b2df699bf985fbd
Samuel Ogbaje <samuelogbaje@gmail.com>
2024-06-09 22:48:19 +0100

    fixed the search for repository

--- src/components/RepoApi.vue
+++ src/components/RepoApi.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container flex flex-col">
 
-    <div class="flex flex-row text-center items-center mx-auto">
+    <div class="flex flex-row gap-4  text-center items-center mx-auto">
       <label for="search">Search Repository</label>
-      <input type="search" name="search" id="" placeholder="Search Repositories"  @keydown ='filterRepositories(repo)'  class="p-2 text-slate-200 rounded-xl">
+      <input type="search" name="search" id="" placeholder="Search Repositories"  @keydown ='filterRepositories'  class="p-2 text-slate-200 rounded-xl">
     </div>
 
    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
@@ -14,15 +14,18 @@
     >
       
         <h1 class="font-bold text-2xl">
-          <a :href="repo.html_url">{{ repo.name }}</a>
+          {{ repo.name }}
         </h1> <br>
         <span>Date : {{ repo.created_at }}</span> <br>
         <span class="text-green-400">Language : {{ repo.language }}</span> <br>
         <span class="my-2">Stars : {{ repo.stargazers_count }}</span> <br>
         <span class="my-2">Visibility : {{ repo.visibility }}</span>
-        <div>
+        <div class="flex flex-row justify-center gap-2">
+           <span class="flex flex-row justify-evenly gap-4 mt-6">Visit
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>    
+            </span>
           <Button class="bg-green-300 text-black my-4  p-2 rounded-lg">
-            <a href="{repo.repos_url}" target="_blank">View More</a>
+            <a :href="repo.html_url" target="_blank"> {{ repo.name }}</a>
           </Button>
         </div>
       </div>
